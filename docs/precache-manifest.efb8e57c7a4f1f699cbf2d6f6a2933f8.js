370a8b26840f17723134bff5269e9ae8ed4cd80d
rocioferro <fer.rodriguezrocio@gmail.com>
2019-09-24 09:58:48 +0200

    publish in github pages

---
+++ docs/precache-manifest.efb8e57c7a4f1f699cbf2d6f6a2933f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5944830fe40f33570cae590441c36330",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bff829decd13eb5c36f8",
+    "url": "./static/css/main.f989ad4d.chunk.css"
+  },
+  {
+    "revision": "3ad08e2b3f80ec03bb15",
+    "url": "./static/js/2.655c466d.chunk.js"
+  },
+  {
+    "revision": "bff829decd13eb5c36f8",
+    "url": "./static/js/main.27689ac2.chunk.js"
+  },
+  {
+    "revision": "8393f0bc52f60a1b0799",
+    "url": "./static/js/runtime-main.8b19de8f.js"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "./static/media/logo.75509f2e.png"
+  },
+  {
+    "revision": "8f337d16b41380fa7ebad95a4b3894e0",
+    "url": "./static/media/space.8f337d16.jpg"
+  }
+]);
