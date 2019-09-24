46b3885caaf29ccf0f0d4964cf236434f903eee9
rocioferro <fer.rodriguezrocio@gmail.com>
2019-09-24 08:39:47 +0200

    change classname on character detail

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -1,6 +1,7 @@
 import React from 'react'; 
 import {Link} from 'react-router-dom';
 
+
 const CharacterDetail = props => {
   const {routerProps, data} = props; 
   const ID = parseInt(routerProps.match.params.id);
@@ -8,13 +9,13 @@ const CharacterDetail = props => {
   if (rick) {
   return(
     <React.Fragment>
-      <div className="rick_card">
-        <img src={rick.image} alt={rick.name} className="rick_picture"></img>
-        <h2 className="rick_name">{rick.name}</h2>
-        <h3 className="rick_species">Status: {rick.status}</h3>
+      <div className="rick_card-detail">
+        <img src={rick.image} alt={rick.name} className="rick_picture-detail"></img>
+        <h2 className="rick_name-detail">{rick.name}</h2>
+        <h3 className="rick_status">Status: {rick.status}</h3>
         <h3 className="rick_species">Species: {rick.species}</h3>
-        <h3 className="rick_species">Origin: {rick.origin.name}</h3>
-        <h3 className="rick_species">Episodes: {rick.episode.length}</h3>
+        <h3 className="rick_origin">Origin: {rick.origin.name}</h3>
+        <h3 className="rick_episode">Episodes: {rick.episode.length}</h3>
       </div>
 
     <Link to="/">Volver</Link>
